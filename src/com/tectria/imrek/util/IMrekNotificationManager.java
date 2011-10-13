7c76c7eaff462b5719df75aebe678f34cee7d1df
Ryan Merl <the.anti.9@gmail.com>
2011-10-13 10:27:32 -0400

    added check to see if channel should be notified or not. added getNotificationObject method

--- src/com/tectria/imrek/util/IMrekNotificationManager.java
+++ src/com/tectria/imrek/util/IMrekNotificationManager.java
@@ -41,20 +41,37 @@ public class IMrekNotificationManager {
 	}
 	
 	public void notifyNewMessage(String chan) {
-		this.showNotification(channelsToNotify.indexOf(chan), "New Message", "You have new messages in " + chan);
+		int id = channelsToNotify.indexOf(chan);
+		if (id > -1) {
+			this.showNotification(id, "New Message", "You have new messages in " + chan, IMrekActivity.class);
+		}
 	}
 	
 	public void removeNotifsForChan(String chan) {
 		notifyMan.cancel(channelsToNotify.indexOf(chan));
 	}
 	
-	private void showNotification(int notifId, String title, String text) {
+	public Notification getNotificationObject(String title, String text, Class intentClass) {
+		Notification n = new Notification();
+		
+		n.flags |= Notification.DEFAULT_ALL;
+		n.icon = com.tectria.imrek.R.drawable.icon;
+		n.when = System.currentTimeMillis();
+		
+		PendingIntent pi = PendingIntent.getActivity(context, 0,  new Intent(context, intentClass), 0);
+		n.setLatestEventInfo(context, title, text, pi);
+		
+		return n;
+	}
+	
+	
+	private void showNotification(int notifId, String title, String text, Class intentClass) {
 		Notification n = new Notification();
 		n.flags |= Notification.DEFAULT_ALL;
 		n.icon = com.tectria.imrek.R.drawable.icon;
 		n.when = System.currentTimeMillis();
 		
-		PendingIntent pi = PendingIntent.getActivity(context, 0,  new Intent(context, IMrekActivity.class), 0);
+		PendingIntent pi = PendingIntent.getActivity(context, 0,  new Intent(context, intentClass), 0);
 		n.setLatestEventInfo(context, title, text, pi);
 		
 		notifyMan.notify(notifId, n);

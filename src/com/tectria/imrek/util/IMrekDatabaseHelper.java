03cd1292d0f63b9aa69cf80b15ca1e9e4f125602
Ryan Merl <the.anti.9@gmail.com>
2011-10-27 14:42:16 -0400

    update database

--- src/com/tectria/imrek/util/IMrekDatabaseHelper.java
+++ src/com/tectria/imrek/util/IMrekDatabaseHelper.java
@@ -7,24 +7,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class IMrekDatabaseHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "IMrekdb";
-	private static final int DATABASE_VERSION = 1;
-	private static final String DATABASE_SQL = "CREATE TABLE channels (_id integer primary key autoincrement, "
-						+ "channel_name text not null);"
-						+ "CREATE TABLE messages(_id integer primary key autoincrement, channel_id integer not null, "
+	private static final int DATABASE_VERSION = 2;
+	private static final String DATABASE_SQL1 = "CREATE TABLE channels (_id integer primary key autoincrement, "
+						+ "channel_name text not null);";
+	private static final String DATABASE_SQL2 = "CREATE TABLE messages(_id integer primary key autoincrement, channel_id integer not null, "
 						+ "username text not null, message text not null, timestamp text not null);";
 	
 	
 	public IMrekDatabaseHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		
-		
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);	
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(DATABASE_SQL);
-		
-
+		db.execSQL(DATABASE_SQL1);
+		db.execSQL(DATABASE_SQL2);
 	}
 
 	@Override
